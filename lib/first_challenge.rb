e0abf0e0a235782e7df3b1e7965fb544979e73f8
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 03:37:15 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,9 @@ def first_challenge
   }
 
   #your code here
-  contacts[:Jon Snow][favorite_icecream_flavors] << 
+  contacts["Fredy Mercury"][favorite_icecream_flavors].each do |flavor|
+    
+  end  
 
 
   #remember to return your newly altered contacts hash!
